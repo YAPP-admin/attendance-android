83c89e9e1f9da9dbf9c8a40ee8f0f8be7fcb3421
jsh-me <ppm_it@naver.com>
2022-02-22 21:30:05 +0900

    implement login screen

--- presentation/src/main/java/com/yapp/presentation/ui/login/screen/Login.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/login/screen/Login.kt
@@ -1,48 +1,71 @@
 package com.yapp.presentation.ui.login.screen
 
-import android.content.Context
-import android.widget.Toast
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material.*
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.yapp.common.util.KakaoTalkLoginProvider
+import com.yapp.presentation.R
 import com.yapp.presentation.ui.login.LoginViewModel
 import com.yapp.presentation.ui.login.state.LoginContract.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-
 @Composable
 fun Login(
     viewModel: LoginViewModel = hiltViewModel(),
 ) {
-    val uiState = viewModel.uiState.collectAsState()
-
     Surface(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
     ) {
-        Column(
-            modifier = Modifier.wrapContentWidth()
-        ) {
-            Button(onClick = {
-                viewModel.setEvent(LoginUiEvent.OnLoginButtonClicked)
-            }) {
-                Text("카카오 로그인")
+        ConstraintLayout {
+            val (skipButton, kakaoTalkLoginButton, introduce) = createRefs()
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(introduce) {
+                        start.linkTo(parent.start)
+                        bottom.linkTo(skipButton.top, 60.dp)
+                        end.linkTo(parent.end)
+                    },
+                text = stringResource(id = R.string.login_attendance_introduce_text)
+            )
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(skipButton) {
+                        start.linkTo(parent.start)
+                        bottom.linkTo(kakaoTalkLoginButton.top, 8.dp)
+                        end.linkTo(parent.end)
+                    },
+                onClick = {
+                    viewModel.setEvent(LoginUiEvent.OnSkipButtonClicked)
+                }
+            ) {
+                Text("넘어가기")
             }
 
-            Button(onClick = {
-                viewModel.setEvent(LoginUiEvent.OnSkipButtonClicked)
-            }) {
-                Text("넘어가기")
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(kakaoTalkLoginButton) {
+                        start.linkTo(parent.start)
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(parent.end)
+                    },
+                onClick = {
+                    viewModel.setEvent(LoginUiEvent.OnLoginButtonClicked)
+                }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login_kakao_login_text)
+                )
             }
         }
     }
