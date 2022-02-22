23c9a0645be61ebd5cc6ad92c797b57d4b342c87
jsh-me <ppm_it@naver.com>
2022-02-23 01:04:20 +0900

    remove comment

--- presentation/src/main/java/com/yapp/presentation/ui/member/main/QRMain.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/main/QRMain.kt
@@ -1,6 +1,5 @@
 package com.yapp.presentation.ui.member.main
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -8,22 +7,16 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.yapp.presentation.ui.member.main.QRMainContract.*
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @Composable
 fun Main(
     scaffoldState: ScaffoldState = rememberScaffoldState(),
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
     viewModel: QRMainViewModel = hiltViewModel(),
 ) {
-    val context = LocalContext.current
     val uiState = viewModel.uiState.collectAsState()
 
     Scaffold(
@@ -35,7 +28,7 @@ fun Main(
             viewModel.effect.collect { effect ->
                 when (effect) {
                     is QRMainUiSideEffect.ShowToast -> {
-                        Toast.makeText(context, effect.msg, Toast.LENGTH_SHORT).show()
+                        scaffoldState.snackbarHostState.showSnackbar("Button Clicked")
                     }
                 }
             }
@@ -47,11 +40,7 @@ fun Main(
             Button(
                 modifier = Modifier.wrapContentHeight(),
                 onClick = {
-                    // viewModel.setEvent(QRMainUiEvent.OnButtonClicked)
-                    // todo: 이런 경우에는 어떻게 처리해야 할까?
-                    coroutineScope.launch {
-                        scaffoldState.snackbarHostState.showSnackbar("Button Clicked")
-                    }
+                    viewModel.setEvent(QRMainUiEvent.OnButtonClicked)
                 }
             ) {
                 Text("Click!")
