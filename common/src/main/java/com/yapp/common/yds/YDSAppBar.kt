c15fb7c512abd3d7093be4a1aea7236e928bd039
jsh-me <ppm_it@naver.com>
2022-02-24 02:21:47 +0900

    fix app bar styles

--- common/src/main/java/com/yapp/common/yds/YDSAppBar.kt
+++ common/src/main/java/com/yapp/common/yds/YDSAppBar.kt
@@ -8,17 +8,32 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yapp.common.theme.AttendanceTheme
+import com.yapp.common.theme.Gray_1200
+import com.yapp.common.theme.Pretendard
 
 @Composable
 fun YDSAppBar(
     title: String = "",
+    backgroundColor: Color = Color.White,
     onClickBackButton: () -> Unit,
     onClickSettings: (() -> Unit)? = null
 ) {
     TopAppBar(
-        title = { Text(text = title) },
+        title = { Text(
+            text = title,
+            color = Gray_1200,
+            fontFamily = Pretendard,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ) },
+        backgroundColor = backgroundColor,
+        elevation = 0.dp,
         navigationIcon = {
             IconButton(onClick = { onClickBackButton() }) {
                 //화살표로 변경이 필요함.
