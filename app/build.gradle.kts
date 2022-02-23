7fa16aaed9d90fa6f5b0085e535d42d2239ee23a
junieberry <junyoung44@g.skku.edu>
2022-02-24 02:51:40 +0900

    add junit dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,10 +11,10 @@ android {
 
     defaultConfig {
         applicationId = Configs.APPLICATION_ID
-        minSdk        = Configs.MIN_SDK
-        targetSdk     = Configs.TARGET_SDK
-        versionCode   = Configs.VERSION_CODE
-        versionName   = Configs.VERSION_NAME
+        minSdk = Configs.MIN_SDK
+        targetSdk = Configs.TARGET_SDK
+        versionCode = Configs.VERSION_CODE
+        versionName = Configs.VERSION_NAME
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -77,4 +77,8 @@ dependencies {
     implementation(platform(Dependencies.Firebase.BOM))
     implementation(Dependencies.Firebase.ANALYTICS_KTX)
     implementation(Dependencies.Firebase.CONFIG_KTX)
+
+    implementation(Dependencies.Test.JUNIT)
+    implementation(Dependencies.Test.ANDROID_JUNIT)
+    implementation(Dependencies.Test.ESPRESSO_CORE)
 }
