4ee196101a0aeee11205018d2ee49e36a34f88ea
jionchu <fn0809@gmail.com>
2022-02-28 11:01:46 +0900

    member setting screen 버전 정보 표시

--- presentation/src/main/java/com/yapp/presentation/ui/member/setting/MemberSetting.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/setting/MemberSetting.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -94,6 +95,9 @@ private fun MenuList() {
     Column(
         modifier = Modifier.padding(top = 28.dp, bottom = 28.dp)
     ) {
+        val context = LocalContext.current
+        val versionName = context.packageManager.getPackageInfo(context.packageName, 0).versionName
+
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -105,7 +109,7 @@ private fun MenuList() {
                 color = Gray_1200,
             )
             Text(
-                text = "1.01.1",
+                text = versionName,
                 color = Gray_600,
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.End
