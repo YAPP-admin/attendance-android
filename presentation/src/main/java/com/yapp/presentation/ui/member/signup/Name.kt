c3b695a6b1238fc11de2b01caa44ac79d3ff3dec
junieberry <junyoung44@g.skku.edu>
2022-03-04 04:41:40 +0900

    이름 입력 창 추가..

--- presentation/src/main/java/com/yapp/presentation/ui/member/signup/Name.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/signup/Name.kt
@@ -3,11 +3,13 @@ package com.yapp.presentation.ui.member.signup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +17,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.yapp.common.theme.AttendanceTypography
 import com.yapp.common.theme.Gray_200
 import com.yapp.common.theme.Gray_800
+import com.yapp.common.theme.Yapp_Orange
 import com.yapp.common.yds.YDSAppBar
 import com.yapp.presentation.R
 
@@ -34,13 +37,16 @@ fun Name(
                 .fillMaxSize()
                 .padding(horizontal = 24.dp)
         ) {
-            InputName()
+            Column() {
+                Title()
+                InputName()
+            }
         }
     }
 }
 
 @Composable
-fun InputName() {
+fun Title() {
     Column() {
 
         var text by remember { mutableStateOf("") }
@@ -62,35 +68,50 @@ fun InputName() {
 }
 
 @Composable
-fun NameTextFieldBackground() {
-    Box(
-        modifier = Modifier
-            .wrapContentHeight()
-            .fillMaxWidth()
-            .background(color = Gray_200, shape = RoundedCornerShape(50.dp))
+fun InputName() {
+
+    var text by remember { mutableStateOf("") }
+
+    BasicTextField(
+        value = text,
+        onValueChange = { text = it },
+        singleLine = true,
+        textStyle = AttendanceTypography.body1,
+        cursorBrush = SolidColor(Yapp_Orange),
+        decorationBox = { innerTextField ->
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Gray_200, shape = RoundedCornerShape(50.dp))
+            ) {
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.width(20.dp))
+                    if (text.isBlank()) {
+                        Text(
+                            text = stringResource(id = R.string.name_example_hint),
+                            color = Gray_200,
+                            style = AttendanceTypography.body1
+                        )
+                    }
+                    else {
+                        innerTextField()
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+
+            }
+        }
     )
 }
 
 @Preview(
-    showBackground = true,
     widthDp = 320,
 )
 @Composable
 fun P() {
 
-    var text by remember { mutableStateOf("") }
 
-    BaseTextField()
-//    TextField(
-//        value = text,
-//        onValueChange = { text = it },
-//        textStyle = AttendanceTypography.body1,
-//        placeholder = {
-//            Text(
-//                text = stringResource(id = R.string.name_example_hint),
-//                style = AttendanceTypography.body1,
-//                color = Gray_400
-//            )
-//        },
-//    )
 }
