8ade83fa15c39a497dd760a01341ca1df3547351
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-03-14 23:44:58 +0900

    fix: 기존에 사용하는 TeamModel을 Team으로 변경
    - Base코드가 merge됨에 따라 변경

--- presentation/src/main/java/com/yapp/presentation/ui/member/signup/TeamContract.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/signup/TeamContract.kt
@@ -3,19 +3,19 @@ package com.yapp.presentation.ui.member.signup
 import com.yapp.common.base.UiEvent
 import com.yapp.common.base.UiSideEffect
 import com.yapp.common.base.UiState
-import com.yapp.presentation.model.TeamModel
+import com.yapp.presentation.model.Team
 
 class TeamContract {
     data class TeamUiState(
         val isLoading: Boolean = false,
-        val teams: List<TeamModel> = emptyList(),
-        val selectedTeam: TeamModel = TeamModel()
+        val teams: List<Team> = emptyList(),
+        val selectedTeam: Team = Team()
     ) : UiState
 
     sealed class TeamSideEffect : UiSideEffect {}
 
     sealed class TeamUiEvent : UiEvent {
-        data class ChooseTeam(val teamType: String) : TeamUiEvent()
+        data class ChooseTeam(val platformType: String) : TeamUiEvent()
         data class ChooseTeamNumber(val teamNum: Int) : TeamUiEvent()
     }
 }
