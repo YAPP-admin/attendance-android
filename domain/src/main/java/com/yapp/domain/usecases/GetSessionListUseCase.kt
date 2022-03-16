64ae7acbd2d7ea83c09829ea5bf2339446e27471
YeongSang Jeon <53253298+toastmeister1@users.noreply.github.com>
2022-03-16 23:00:59 +0900

    Fix/main admin (#75)
    * fix: UseCase의 Param이 없어도 되게끔 수정
    
    * feat: GetSessionListUseCase 구현
    
    * fix: Firebase RemoteConfig 값이 흐르지 않는 오류 수정
    
    이미 remoteConfig가 활성화된 경우에는 emit을 하지 않는 문제가 있었음
    hadActive를 분기하는것을 다시 재고해보니 의미가 없는 행위로 삭제
    어떤 경우에든 값을 emit하도록 수정
    
    * refactor: 리뷰반영
    
    - Hilt provide -> binds
    - BaseUsecase Type fixed
    
    * fix: MemberScore Session을 가져오는 부분 Usecase로 복원

--- domain/src/main/java/com/yapp/domain/usecases/GetSessionListUseCase.kt
+++ domain/src/main/java/com/yapp/domain/usecases/GetSessionListUseCase.kt
@@ -1,19 +1,19 @@
 package com.yapp.domain.usecases
 
 import com.yapp.domain.firebase.FirebaseRemoteConfig
-import com.yapp.domain.model.TeamEntity
+import com.yapp.domain.model.SessionEntity
 import com.yapp.domain.util.BaseUseCase
 import com.yapp.domain.util.TaskResult
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class GetTeamListUseCase @Inject constructor(
+class GetSessionListUseCase @Inject constructor(
     private val firebaseRemoteConfig: FirebaseRemoteConfig
-) : BaseUseCase<@JvmSuppressWildcards Flow<TaskResult<List<TeamEntity>>>, Unit>() {
+) : BaseUseCase<@JvmSuppressWildcards Flow<TaskResult<List<SessionEntity>>>, Unit>() {
 
-    override suspend fun invoke(params: Unit?): Flow<TaskResult<List<TeamEntity>>> {
-        return firebaseRemoteConfig.getTeamList()
+    override suspend fun invoke(params: Unit?): Flow<TaskResult<List<SessionEntity>>> {
+        return firebaseRemoteConfig.getSessionList()
             .toResult()
     }
 
--- presentation/src/main/java/com/yapp/presentation/ui/admin/main/AdminMainViewModel.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/admin/main/AdminMainViewModel.kt
@@ -1,15 +1,33 @@
 package com.yapp.presentation.ui.admin.main
 
+import androidx.lifecycle.viewModelScope
 import com.yapp.common.base.BaseViewModel
+import com.yapp.domain.usecases.GetSessionListUseCase
+import com.yapp.presentation.model.Session.Companion.mapTo
 import com.yapp.presentation.ui.admin.main.AdminMainContract.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AdminMainViewModel @Inject constructor(
-) : BaseViewModel<AdminMainUiState, AdminMainUiSideEffect, AdminMainUiEvent>(
-    AdminMainUiState()
-) {
+    private val getSessionListUseCase: GetSessionListUseCase
+) : BaseViewModel<AdminMainUiState, AdminMainUiSideEffect, AdminMainUiEvent>(AdminMainUiState()) {
+
+    init {
+        viewModelScope.launch {
+            getSessionListUseCase()
+                .collectWithCallback(
+                    onSuccess = { entities ->
+                        val sessions = entities.map { entity -> entity.mapTo() }
+                        setState { copy(isLoading = false, sessions = sessions) }
+                    },
+                    onFailed = {
+                        // TODO 에러핸들링 필요합니다.
+                    }
+                )
+        }
+    }
 
     override fun setEvent(event: AdminMainUiEvent) {
         super.setEvent(event)
@@ -18,4 +36,5 @@ class AdminMainViewModel @Inject constructor(
     override fun handleEvent(event: AdminMainUiEvent) {
         TODO("Not yet implemented")
     }
+
 }
