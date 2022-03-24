135902be048f0c297fab844d9af796179586c66d
Jihee.Han <jihee.dev@gmail.com>
2022-03-24 15:38:34 +0900

    출석 상태에 따라 QR 스캔 화면이 달라질 수 있도록 구현합니다 (#78)
    * fix: limit scan area
    
    * design: change permission dialog text
    
    * feat: QR 스캔 전 후 상황에 맞게 화면이 보여지도록 수정
    
    * feat: add toast, box design system
    
    * feat: apply design system to qr screen
    
    * refactor: clean code
    
    rename QrCodeUiState value
    rename CheckState to AttendanceState
    change ScannerDeco layout
    rename QrCodeParser
    
    * refactor: PR 리뷰 반영
    
    디자인 시스템에 없는 색상 코드는 색상 코드로 적용(Color 목록에서 삭제)
    QrUiState에 isLoading 추가
    외부에서 사용되지 않는 함수 private 함수로 변경(intentToAppSetting())
    잘못된 이벤트 처리 방식 수정

---
+++ common/src/main/java/com/yapp/common/yds/YDSToast.kt
@@ -0,0 +1,38 @@
+package com.yapp.common.yds
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.yapp.common.theme.AttendanceTypography
+
+@Composable
+fun YDSToast(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 24.dp)
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(color = Color(0XCC2C2F35))
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            style = AttendanceTypography.body1,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
--- presentation/src/main/java/com/yapp/presentation/ui/member/qrcodescanner/QrCodeAnalyzer.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/qrcodescanner/QrCodeAnalyzer.kt
@@ -1,6 +1,7 @@
 package com.yapp.presentation.ui.member.qrcodescanner
 
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
@@ -11,16 +12,20 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class QrCodeAnalyzer @Inject constructor(
-    private val onQrCodeDetected: (List<Barcode>) -> Unit,
+    private val onQrCodeDetected: (Barcode) -> Unit,
     private val onFailToAnalysis: (Exception) -> Unit
 ) : ImageAnalysis.Analyzer {
-    private val SCAN_DURATION = 3L
+    companion object {
+        const val SCAN_DURATION = 1L
+    }
+
     private var lastAnalyzedTimeStamp = 0L
 
     @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(imageProxy: ImageProxy) {
         val currentTimeStamp = System.currentTimeMillis()
         if (currentTimeStamp - lastAnalyzedTimeStamp >= TimeUnit.SECONDS.toMillis(SCAN_DURATION)) {
+            imageProxy.setCropRect(Rect(0, 0, 200, 200))
             imageProxy.image?.let { imageToAnalyze ->
                 val options = BarcodeScannerOptions.Builder()
                     .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
@@ -28,11 +33,22 @@ class QrCodeAnalyzer @Inject constructor(
                 val qrCodeScanner = BarcodeScanning.getClient(options)
                 val imageToProcess =
                     InputImage.fromMediaImage(imageToAnalyze, imageProxy.imageInfo.rotationDegrees)
+                val availableAreaEdge = imageProxy.height / 2
+                val availableArea = Rect(
+                    (imageProxy.height - availableAreaEdge) / 2,
+                    (imageProxy.width - availableAreaEdge) / 2,
+                    (imageProxy.height + availableAreaEdge) / 2,
+                    (imageProxy.width + availableAreaEdge) / 2
+                ) // 대략 화면 높이의 절반 길이의 정사각형 안으로 들어와야 스캔되도록 구현함
+                // todo: 정확히 스캔 범위 내로 들어와야 스캔되도록 수정
 
                 qrCodeScanner.process(imageToProcess)
                     .addOnSuccessListener { qrCodes ->
-                        if (qrCodes.isNotEmpty()) {
-                            onQrCodeDetected(qrCodes)
+                        if (qrCodes.size == 1) {
+                            val detectedCode = qrCodes[0]
+                            if (availableArea.contains(detectedCode.boundingBox!!)) {
+                                onQrCodeDetected(detectedCode)
+                            }
                         }
                     }
                     .addOnFailureListener { exception ->
