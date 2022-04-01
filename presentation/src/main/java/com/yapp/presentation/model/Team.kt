cc06dc9d7553a15bcc57fb68a9c8361a62fee28c
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-04-01 22:04:18 +0900

    fix: Team의 필드 명칭을 좀 더 명확하게 변경

--- presentation/src/main/java/com/yapp/presentation/model/Team.kt
+++ presentation/src/main/java/com/yapp/presentation/model/Team.kt
@@ -1,17 +1,17 @@
 package com.yapp.presentation.model
 
 import com.yapp.domain.model.TeamEntity
-import com.yapp.presentation.model.type.PlatformType
+import com.yapp.presentation.model.type.TeamType
 
 data class Team(
-    val platform: PlatformType? = null,
+    val type: TeamType? = null,
     val number: Int? = null,
 ) {
 
     companion object {
         fun TeamEntity.mapTo(): Team {
             return Team(
-                platform = PlatformType.of(platform),
+                type = TeamType.of(type),
                 number = number
             )
         }
