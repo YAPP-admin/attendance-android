0c40ec36fe7c6bdacd8b1dd16676983ae52039bd
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-04-01 21:49:32 +0900

    feat: Session에 휴얍여부 필드 추가

--- domain/src/main/java/com/yapp/domain/model/SessionEntity.kt
+++ domain/src/main/java/com/yapp/domain/model/SessionEntity.kt
@@ -4,6 +4,7 @@ package com.yapp.domain.model
 data class SessionEntity(
     val sessionId: Int,
     val title: String,
+    val isOff: Boolean,
     val date: String,
     val description: String
 )
