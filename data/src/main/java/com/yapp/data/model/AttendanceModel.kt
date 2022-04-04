81d9cecc150380d47b38d1f9574c0017e1ad3688
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-04-05 01:46:06 +0900

    Fix: AttendanceType 기획 변경에 따른 수정

--- data/src/main/java/com/yapp/data/model/AttendanceModel.kt
+++ data/src/main/java/com/yapp/data/model/AttendanceModel.kt
@@ -24,22 +24,20 @@ data class AttendanceModel(
 }
 
 @Serializable
-data class AttendanceTypeModel(val point: Int = 0, val text: String = ABSENT) {
+data class AttendanceTypeModel(val point: Int = -20, val text: String = ABSENT) {
 
     companion object {
-        private const val ABSENT = "미통보 결석"
-        private const val REPORTED_ABSENT = "결석"
+        private const val ABSENT = "결석"
         private const val NORMAL = "출석"
-        private const val LATE = "미통보 지각"
-        private const val REPORTED_LATE = "지각"
+        private const val LATE = "지각"
+        private const val ADMIT = "출석 인정"
 
         fun AttendanceTypeModel.mapToEntity(): AttendanceTypeEntity {
             return when (this.text) {
                 ABSENT -> AttendanceTypeEntity.Absent
-                REPORTED_ABSENT -> AttendanceTypeEntity.ReportedAbsent
                 NORMAL -> AttendanceTypeEntity.Normal
                 LATE -> AttendanceTypeEntity.Late
-                REPORTED_LATE -> AttendanceTypeEntity.ReportedLate
+                ADMIT -> AttendanceTypeEntity.Admit
                 else -> error("알 수 없는 AttendanceType입니다.")
             }
         }
