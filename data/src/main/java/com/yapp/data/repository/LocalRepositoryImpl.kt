4685d100265dc3260596a476be3b5d721e236697
Junyoung Kim <junyoung44@g.skku.edu>
2022-04-04 16:13:33 +0900

    LoginViewModel에 member id set 로직 추가

--- data/src/main/java/com/yapp/data/repository/LocalRepositoryImpl.kt
+++ data/src/main/java/com/yapp/data/repository/LocalRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.yapp.data.repository
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
--- presentation/src/main/java/com/yapp/presentation/ui/login/LoginViewModel.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/login/LoginViewModel.kt
@@ -1,15 +1,20 @@
 package com.yapp.presentation.ui.login
 
+import androidx.lifecycle.viewModelScope
 import com.yapp.common.base.BaseViewModel
 import com.yapp.common.util.KakaoSdkProvider
 import com.yapp.domain.common.KakaoSdkProviderInterface
+import com.yapp.domain.usecases.SetMemberIdUseCase
 import com.yapp.presentation.ui.login.LoginContract.*
+import com.yapp.presentation.ui.splash.SplashContract
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val kakaoSdkProvider: KakaoSdkProviderInterface
+    private val kakaoSdkProvider: KakaoSdkProviderInterface,
+    private val setMemberIdUseCase: SetMemberIdUseCase
 ) :
     BaseViewModel<LoginUiState, LoginUiSideEffect, LoginUiEvent>(
         LoginUiState()
@@ -23,13 +28,24 @@ class LoginViewModel @Inject constructor(
                 }
                 kakaoSdkProvider.login(
                     onSuccess = {
-                        setEffect(
-                            LoginUiSideEffect.NavigateToSignUpScreen,
-                            LoginUiSideEffect.ShowToast("로그인 성공")
+                        kakaoSdkProvider.validateAccessToken(
+                            onSuccess = { userAccountId ->
+                                viewModelScope.launch { setMemberIdUseCase(userAccountId) }
+                                setEffect(
+                                    LoginUiSideEffect.NavigateToSignUpScreen,
+                                    LoginUiSideEffect.ShowToast("로그인 성공")
+                                )
+                                setState {
+                                    copy(isLoading = false)
+                                }
+
+                            },
+                            onFailed = { setEffect(
+                                LoginUiSideEffect.ShowToast("로그인 실패")
+                            ) }
                         )
-                        setState {
-                            copy(isLoading = false)
-                        }
+
+
                     },
                     onFailed = {
                         setEffect(LoginUiSideEffect.ShowToast("로그인 실패"))
