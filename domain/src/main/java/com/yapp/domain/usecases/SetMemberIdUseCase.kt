5b3d4db034c2b30be742a8448c95c605fce52e54
Junyoung Kim <junyoung44@g.skku.edu>
2022-04-04 15:33:43 +0900

    conflict 해결

--- domain/src/main/java/com/yapp/domain/usecases/SetMemberIdUseCase.kt
+++ domain/src/main/java/com/yapp/domain/usecases/SetMemberIdUseCase.kt
@@ -2,13 +2,15 @@ package com.yapp.domain.usecases
 
 import com.yapp.domain.repository.LocalRepository
 import com.yapp.domain.util.BaseUseCase
+import com.yapp.domain.util.DispatcherProvider
 import com.yapp.domain.util.TaskResult
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SetMemberIdUseCase @Inject constructor(
     private val localRepository: LocalRepository,
-) : BaseUseCase<Flow<TaskResult<Unit?>>, Long?>() {
+    private val coroutineDispatcher: DispatcherProvider
+) : BaseUseCase<Flow<TaskResult<Unit?>>, Long?>(coroutineDispatcher) {
     override suspend fun invoke(params: Long?): Flow<TaskResult<Unit>> {
         return localRepository.setMemberId(params!!).toResult()
     }
