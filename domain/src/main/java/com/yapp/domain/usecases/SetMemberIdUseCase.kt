258b83dd0c4221a0244715e5591e73955595aa82
jsh-me <ppm_it@naver.com>
2022-04-06 04:05:33 +0900

    로그인 시 Member id DataStore 에 저장할 수 있도록 개선

--- domain/src/main/java/com/yapp/domain/usecases/SetMemberIdUseCase.kt
+++ domain/src/main/java/com/yapp/domain/usecases/SetMemberIdUseCase.kt
@@ -3,15 +3,13 @@ package com.yapp.domain.usecases
 import com.yapp.domain.repository.LocalRepository
 import com.yapp.domain.util.BaseUseCase
 import com.yapp.domain.util.DispatcherProvider
-import com.yapp.domain.util.TaskResult
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SetMemberIdUseCase @Inject constructor(
     private val localRepository: LocalRepository,
     private val coroutineDispatcher: DispatcherProvider
-) : BaseUseCase<Flow<TaskResult<Unit?>>, Long?>(coroutineDispatcher) {
-    override suspend fun invoke(params: Long?): Flow<TaskResult<Unit>> {
-        return localRepository.setMemberId(params!!).toResult()
+) : BaseUseCase<Unit, Long?>(coroutineDispatcher) {
+    override suspend fun invoke(params: Long?) {
+        localRepository.setMemberId(params!!)
     }
 }
