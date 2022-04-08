4c33db798d3ec2cb99074b2211c6fd68b8aabf5b
Jihee.Han <jihee.dev@gmail.com>
2022-04-08 19:27:57 +0900

    QR 로티 및 프로그레스 바 배경을 수정합니다 (#97)
    * feat: QR 체크 성공 시 로티 애니메이션 추가
    
    * design: set progress bar background opacity
    
    * feat: QR 스캐너에 로딩 화면 추가
    
    * fix: set navigation start destination
    
    * design: QR 인증 성공 후 딜레이 짧은 파일로 교체

--- presentation/src/main/java/com/yapp/presentation/ui/AttendanceScreen.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/AttendanceScreen.kt
@@ -1,15 +1,14 @@
 package com.yapp.presentation.ui
 
-import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.*
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -21,13 +20,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.yapp.common.R
 import com.yapp.common.theme.Yapp_Orange
 import com.yapp.common.yds.YDSToast
 import com.yapp.presentation.ui.admin.browse.AdminTotalScore
 import com.yapp.presentation.ui.admin.main.AdminMain
 import com.yapp.presentation.ui.login.Login
-import com.yapp.presentation.ui.login.LoginContract
 import com.yapp.presentation.ui.member.help.Help
 import com.yapp.presentation.ui.member.main.MemberMain
 import com.yapp.presentation.ui.member.privacy_policy.PrivacyPolicyScreen
