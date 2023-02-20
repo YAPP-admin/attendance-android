d460db1e2d54e3a408893c42cfb9ed0165a5b836
Jeongho <sports7744@icloud.com>
2023-02-20 23:15:15 +0900

    refactor: 옵션 컴포넌트 생성 및 적용 (#199)
    * feat: 옵션 컴포넌트 추가
    
    * refactor: 포지션 화면에 옵션 컴포넌트 적용
    
    * refactor: 팀 화면에 옵션 컴포넌트 적용
    
    * refactor: 컨벤션에 따라 코드 정리
    
    * refactor: 적합하지 않은 포지션 선택 시 메시지를 통해 유저에게 안내하도록 구현
    
    * feat: 옵션 공통 컴포넌트에 대한 State 인터페이스 정의
    
    * refactor: 포지션 선택 화면에서 옵션 공통 컴포넌트 State 를 사용하도록 수정
    
    * refactor: 팀 선택 화면에서 옵션 공통 컴포넌트 State 를 사용하도록 수정
    
    * refactor: YDSOptionState 의 select 메소드 삭제

---
+++ common/src/main/java/com/yapp/common/yds/YDSOption.kt
@@ -0,0 +1,33 @@
+package com.yapp.common.yds
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun YDSOption(types: List<String>, selectedType: String?, onTypeClicked: (String) -> Unit) {
+    val rowNum = 2
+    Column {
+        repeat(types.size / rowNum) { row ->
+            Row {
+                repeat(rowNum) { index ->
+                    val type = types[rowNum * row + index]
+                    YDSChoiceButton(
+                        text = type,
+                        modifier = Modifier.padding(end = 12.dp, bottom = 12.dp),
+                        state = if (selectedType == type) YdsButtonState.ENABLED else YdsButtonState.DISABLED,
+                        onClick = { onTypeClicked(type) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+interface YDSOptionState<T> {
+    val items: List<T>
+    val selectedOption: T?
+}
