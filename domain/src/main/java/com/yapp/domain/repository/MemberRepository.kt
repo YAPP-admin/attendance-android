5469e76144192238eadcbc73b8ab9132abd07dd8
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2023-02-21 18:54:24 +0900

    feat: 실시간 멤버 변동 사항을 받아오는 레파지토리 생성
    * Null 일 때 Exception 방출

--- domain/src/main/java/com/yapp/domain/repository/MemberRepository.kt
+++ domain/src/main/java/com/yapp/domain/repository/MemberRepository.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.Flow
 interface MemberRepository {
     suspend fun setMember(member: Member): Result<Unit>
     suspend fun getMember(id: Long): Result<Member?>
+    suspend fun getMemberWithFlow(id: Long): Flow<Result<Member>>
     suspend fun deleteMember(id: Long): Result<Unit>
     suspend fun getAllMember(): Flow<Result<List<Member>>>
 }
