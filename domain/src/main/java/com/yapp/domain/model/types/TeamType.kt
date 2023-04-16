a668439241d5dd3511ddc448617bf555a9217604
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2023-04-17 01:04:12 +0900

    feat: 팀 미정 타입 추가 및 팀 구조 변경
    * 팀 미정 타입 추가
    * 올라운더 팀 삭제
    * 베이스캠프 팀 추가

--- domain/src/main/java/com/yapp/domain/model/types/TeamType.kt
+++ domain/src/main/java/com/yapp/domain/model/types/TeamType.kt
@@ -5,7 +5,8 @@ enum class TeamType(val value: String) {
     ANDROID("Android"),
     IOS("iOS"),
     WEB("Web"),
-    ALL_ROUNDER("All-Rounder");
+    BASECAMP("BASECAMP"),
+    NONE("None");
 
     companion object {
         fun from(rawValue: String): TeamType {
@@ -13,7 +14,8 @@ enum class TeamType(val value: String) {
                 "ANDROID" -> ANDROID
                 "IOS" -> IOS
                 "WEB" -> WEB
-                "ALL_ROUNDER" -> ALL_ROUNDER
+                "BASECAMP" -> BASECAMP
+                "None" -> NONE
                 else -> error("$rawValue 에 해당하는 TeamType이 없습니다.")
             }
         }
