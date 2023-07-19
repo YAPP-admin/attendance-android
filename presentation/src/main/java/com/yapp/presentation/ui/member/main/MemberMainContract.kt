d8be32666c6b2d022cb0d4ba2b6ba1ee9b03e63a
taeseongyun <yts0646@gmail.com>
2023-07-19 22:47:04 +0900

    [ Feature ] : 멤버 메인 mvi 구조 변경 및 추가

--- presentation/src/main/java/com/yapp/presentation/ui/member/main/MemberMainContract.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/main/MemberMainContract.kt
@@ -7,16 +7,19 @@ import com.yapp.common.base.UiState
 class MemberMainContract {
     data class MemberMainUiState(
         val isLoading: Boolean = true,
-        val showDialog: Boolean = false,
+        val isShowTeamDialog: Boolean = false,
         val selectedTab: BottomNavigationItem = BottomNavigationItem.SESSION,
         val isAttendanceCompleted: Boolean = false,
     ) : UiState
 
     sealed class MemberMainUiSideEffect : UiSideEffect {
         data class NavigateToRoute(val tab: BottomNavigationItem) : MemberMainUiSideEffect()
+        object NavigateToTeam : MemberMainUiSideEffect()
     }
 
     sealed class MemberMainUiEvent : UiEvent {
         data class OnClickBottomNavigationTab(val tab: BottomNavigationItem) : MemberMainUiEvent()
+        object OnNextTime : MemberMainUiEvent()
+        object OnSelectTeamScreen : MemberMainUiEvent()
     }
 }
