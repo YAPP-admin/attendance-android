bd2c47e1d5338871f0855aba157bdbd2136a9e67
taeseongyun <yts0646@gmail.com>
2023-07-19 22:47:47 +0900

    [ Feature ] : 메인 멤버 mvi 구조 변경으로 인한 추가 로직 수정

--- presentation/src/main/java/com/yapp/presentation/ui/member/main/MemberMainViewModel.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/member/main/MemberMainViewModel.kt
@@ -1,15 +1,31 @@
 package com.yapp.presentation.ui.member.main
 
+import androidx.lifecycle.viewModelScope
 import com.yapp.common.base.BaseViewModel
+import com.yapp.domain.model.types.TeamType
+import com.yapp.domain.usecases.GetCurrentMemberInfoUseCase
 import com.yapp.presentation.ui.member.main.MemberMainContract.MemberMainUiEvent
 import com.yapp.presentation.ui.member.main.MemberMainContract.MemberMainUiSideEffect
 import com.yapp.presentation.ui.member.main.MemberMainContract.MemberMainUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MemberMainViewModel @Inject constructor() :
+class MemberMainViewModel @Inject constructor(
+    getMemberGetCurrentMemberInfoUseCase: GetCurrentMemberInfoUseCase
+) :
     BaseViewModel<MemberMainUiState, MemberMainUiSideEffect, MemberMainUiEvent>(MemberMainUiState()) {
+
+    init {
+        viewModelScope.launch {
+            val member = getMemberGetCurrentMemberInfoUseCase.invoke().getOrNull()
+
+            if(member != null) {
+                setState { copy(isShowTeamDialog = member.team.type.value == TeamType.NONE.value) }
+            }
+        }
+    }
 
     override suspend fun handleEvent(event: MemberMainUiEvent) {
         when (event) {
@@ -17,8 +33,15 @@ class MemberMainViewModel @Inject constructor() :
                 setState { copy(selectedTab = event.tab) }
                 setEffect( MemberMainUiSideEffect.NavigateToRoute(event.tab))
             }
+            is MemberMainUiEvent.OnNextTime -> {
+                setState { copy(isShowTeamDialog = false) }
+            }
+            is MemberMainUiEvent.OnSelectTeamScreen -> {
+                setState { copy(isShowTeamDialog = false) }
+                setEffect(MemberMainUiSideEffect.NavigateToTeam)
+            }
+
         }
     }
-
 }
 
