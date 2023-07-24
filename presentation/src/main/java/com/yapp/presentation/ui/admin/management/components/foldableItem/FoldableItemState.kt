bfcdac296dc2af20e73c0a4cc1bbc076a1424e8a
Yeongsang Jeon <53253298+toastmeister1@users.noreply.github.com>
2023-07-24 22:01:43 +0900

    [ Feature ] : 누적 점수 페이지에 정렬 기준 추가 (#302)
    * [ Feature ] : Score Type의 Foldable Item 추가
    
    * [ Feature ] : AdminTotalScore (점수현황) 화면 구현
    
    - 기존 Management화면에서 FoldableItem을 재사용하여 구현합니다.
       - Management / AdminTotalScore의 요구사항에 따라 FoldableItem을 수정합니다.
       - 이에따라 ViewModel의 로직또한 비슷한 형태를 갖추도록 구협합니다.
    - 패딩이 들어간 기존 'icon_warning'을 패딩이 들어가지 않은 이미지로 교체합니다.
    
    * [ Feature ] : YDS Box의 가로 패딩 제거
    
    - 해당 컴포저블을 감싸고 있는 padding정책에 따르도록 수정합니다.
    
    * [ Fix ] : 점수 변경시 Header가 초기화(Collapse) 되는 문제 해결 
    
    - ScoreType을 filter하도록 하여 이전의 State의 HeaderState를 올바르게 가져오도록 합니다.
    
    * [ Fix ] : Contract를 import하여 코드 가독성 개선
    
    * [ Fix ] : 오탈자 수정

--- presentation/src/main/java/com/yapp/presentation/ui/admin/management/components/foldableItem/FoldableItemState.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/admin/management/components/foldableItem/FoldableItemState.kt
@@ -3,12 +3,43 @@ package com.yapp.presentation.ui.admin.management.components.foldableItem
 import FoldableHeaderItemState
 import androidx.compose.runtime.Stable
 import com.yapp.presentation.ui.admin.management.components.foldableItem.foldableContentItem.FoldableContentItemState
+import com.yapp.presentation.ui.admin.management.components.foldableItem.foldableContentItem.FoldableContentItemWithButtonState
+import com.yapp.presentation.ui.admin.management.components.foldableItem.foldableContentItem.FoldableContentItemWithScoreState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 
+/**
+ * ## Description
+ * Foldable Header와 Content를 묶어놓은 State
+ *
+ * -
+ * ## Type
+ * **[[Type 1]]**. [PositionItemWithButtonContentState]
+ * - Tab의 기준 : Position
+ * - Content Type : ButtonType
+ *
+ * **[[Type 2]]**. [TeamItemWithButtonContentState]
+ * - Tab의 기준 : Team
+ * - Content Type : ButtonType []
+ *
+ * **[[Type 3]]**. [PositionItemWithScoreContentState]
+ * - Tab의 기준 : Position
+ * - Content Type : ScoreType
+ *
+ * **[[Type 4]]**. [TeamItemWithScoreContentState]
+ * - Tab의 기준 : Team
+ * - Content Type : ScoreType
+ *
+ * ## Params
+ * @property headerItem FoldableItem의 Header
+ * @property contentItems FoldableItem의 Contents
+ * @property flatten  Contents가 [FoldableHeaderItemState.isExpanded]에 따라 Ui에 표시되는 형태를 나타낸다.
+ *
+ */
 @Stable
 interface FoldableItemState {
+
     val headerItem: FoldableHeaderItemState
     val contentItems: ImmutableList<FoldableContentItemState>
 
@@ -26,25 +57,23 @@ interface FoldableItemState {
 
 }
 
-data class PositionItemState(
+data class PositionItemWithButtonContentState(
     override val headerItem: FoldableHeaderItemState.PositionType,
-    override val contentItems: ImmutableList<FoldableContentItemState.PositionType>
+    override val contentItems: ImmutableList<FoldableContentItemWithButtonState.PositionType>
 ) : FoldableItemState {
 
     val position: String
         get() = headerItem.position
 
     override fun setHeaderItemExpandable(isExpand: Boolean): FoldableItemState {
-        return this.copy(
-            headerItem = headerItem.copy(isExpanded = isExpand)
-        )
+        return this.copy(headerItem = headerItem.copy(isExpanded = isExpand))
     }
 
 }
 
-data class TeamItemState(
+data class TeamItemWithButtonContentState(
     override val headerItem: FoldableHeaderItemState.TeamType,
-    override val contentItems: ImmutableList<FoldableContentItemState.TeamType>
+    override val contentItems: ImmutableList<FoldableContentItemWithButtonState.TeamType>
 ) : FoldableItemState {
 
     val teamName: String
@@ -54,9 +83,38 @@ data class TeamItemState(
         get() = headerItem.teamNumber
 
     override fun setHeaderItemExpandable(isExpand: Boolean): FoldableItemState {
-        return this.copy(
-            headerItem = headerItem.copy(isExpanded = isExpand)
-        )
+        return this.copy(headerItem = headerItem.copy(isExpanded = isExpand))
+    }
+
+}
+
+data class PositionItemWithScoreContentState(
+    override val headerItem: FoldableHeaderItemState.PositionType,
+    override val contentItems: ImmutableList<FoldableContentItemWithScoreState.PositionType>
+) : FoldableItemState {
+
+    val position: String
+        get() = headerItem.position
+
+    override fun setHeaderItemExpandable(isExpand: Boolean): FoldableItemState {
+        return this.copy(headerItem = headerItem.copy(isExpanded = isExpand))
+    }
+
+}
+
+data class TeamItemWithScoreContentState(
+    override val headerItem: FoldableHeaderItemState.TeamType,
+    override val contentItems: ImmutableList<FoldableContentItemWithScoreState.TeamType>
+) : FoldableItemState {
+
+    val teamName: String
+        get() = headerItem.teamName
+
+    val teamNumber: Int
+        get() = headerItem.teamNumber
+
+    override fun setHeaderItemExpandable(isExpand: Boolean): FoldableItemState {
+        return this.copy(headerItem = headerItem.copy(isExpanded = isExpand))
     }
 
 }
---
+++ presentation/src/main/java/com/yapp/presentation/ui/admin/totalscore/dto/AdminTotalScoreSharedData.kt
@@ -0,0 +1,4 @@
+package com.yapp.presentation.ui.admin.totalscore.dto
+
+
+data class AdminTotalScoreSharedData(val sessionId: Int = 0)
