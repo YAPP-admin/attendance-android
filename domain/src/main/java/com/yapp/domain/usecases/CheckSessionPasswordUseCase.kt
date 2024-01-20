c4ed6912dcd629a918d764281106b41ce6c444de
YeongSang Jeon <wjsdudtkd95@gmail.com>
2024-01-20 18:04:28 +0900

    출석시 Session 모델에 담긴 Code로 출석하도록 수정
    - RemoteConfig에 존재하는 공통의 출석코드가 아닌 각 세션에 존재하는 Code를 Input과 비교하여 출석하도록 수정

--- domain/src/main/java/com/yapp/domain/usecases/CheckSessionPasswordUseCase.kt
+++ domain/src/main/java/com/yapp/domain/usecases/CheckSessionPasswordUseCase.kt
@@ -1,14 +1,16 @@
 package com.yapp.domain.usecases
 
-import com.yapp.domain.repository.RemoteConfigRepository
+import com.yapp.domain.repository.SessionRepository
 import javax.inject.Inject
 
 class CheckSessionPasswordUseCase @Inject constructor(
-    private val remoteConfigRepository: RemoteConfigRepository,
+    private val sessionRepository: SessionRepository
 ) {
-    suspend operator fun invoke(inputPassword: String): Result<Boolean> {
-        return remoteConfigRepository.getSessionPassword().mapCatching { sessionPassword: String ->
-            inputPassword == sessionPassword
+    suspend operator fun invoke(sessionId: Int, inputPassword: String): Result<Boolean> {
+        return sessionRepository.getSession(id = sessionId).mapCatching { session ->
+            check(session != null) { "${sessionId}에 해당하는 Session이 존재하지 않습니다" }
+
+            inputPassword == session.code
         }
     }
 }
