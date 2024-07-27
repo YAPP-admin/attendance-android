00dc8c989c06a1865c1a9a97ebb71a6f6bbc5dc5
YeongSang Jeon <wjsdudtkd95@gmail.com>
2024-07-27 17:49:50 +0900

    Target SDK 버전 업
    구글 정책상 업데이트 33 -> 34

--- buildSrc/src/main/java/com/yapp/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/yapp/buildsrc/Configs.kt
@@ -17,7 +17,7 @@ object Configs {
     //Android Default Config
     const val APPLICATION_ID            = "com.yapp.attendance"
     const val MIN_SDK                   = 26
-    const val TARGET_SDK                = 33
+    const val TARGET_SDK                = 34
     const val VERSION_CODE              = 100402
     const val VERSION_NAME              = "1.4.2"
 
