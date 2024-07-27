99971b4401cb23059972b8f71470c80c2bacd66d
YeongSang Jeon <wjsdudtkd95@gmail.com>
2024-07-27 19:17:43 +0900

    chore: Version Up (1.4.2 -> 1.4.3) 🔥🔥🔥

--- buildSrc/src/main/java/com/yapp/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/yapp/buildsrc/Configs.kt
@@ -18,8 +18,8 @@ object Configs {
     const val APPLICATION_ID            = "com.yapp.attendance"
     const val MIN_SDK                   = 26
     const val TARGET_SDK                = 34
-    const val VERSION_CODE              = 100402
-    const val VERSION_NAME              = "1.4.2"
+    const val VERSION_CODE              = 100403
+    const val VERSION_NAME              = "1.4.3"
 
     const val KOTLIN_COMPILER_EXTENSION = "1.4.3"
 }
